12be55915aa4549eeaf0b9f5da1930b70334e5f0
Dibyarup Pal <dibyaruppal17@gmail.com>
2023-08-25 12:48:15 +0530

    '

--- Hands on List 1/11.c
+++ Hands on List 1/11.c
@@ -8,6 +8,7 @@ int main(){
 		printf("Error opening dup_test.txt || \n");
 		return 1;
 	}
+	printf("fd -> %d\n",fd);
 
 	char content1[] = "1. Hello World !!\n";
 	int fd_wr = write(fd,content1,sizeof(content1)-1);
@@ -23,6 +24,7 @@ int main(){
 		close(fd);
 		return 1;
 	}
+	printf("dup_fd -> %d\n",dup_fd);
 
 	char content2[] = "2. Hello World !!\n";
 	int dup_fd_wr = write(dup_fd,content2,sizeof(content2)-1);
@@ -40,32 +42,30 @@ int main(){
 		close(dup_fd);
 		return 1;
 	}
+	printf("new_fd -> %d\n",new_fd);
 	
-	int dup2_fd;
-	if(dup2(dup2_fd,new_fd) == -1){
+	if(dup2(dup_fd,new_fd) == -1){
 		printf("Error duplicating using dup2 !!\n");
 		close(fd);
 		close(dup_fd);
 		close(new_fd);
 		return 1;
 	}
+	printf("new_fd -> %d\n",new_fd);
 
 	char content3[] = "3. Hello World !!\n";
-	int dup2_fd_wr = write(dup2_fd,content3,sizeof(content3)-1);
+	int dup2_fd_wr = write(new_fd,content3,sizeof(content3)-1);
 	if(dup2_fd_wr == -1){
 		printf("Error writing content3 to dup2_test.txt");
 		close(fd);
 		close(dup_fd);
 		close(new_fd);
-		close(dup2_fd);
 		return 1;
 	}
 
 	close(fd);
 	close(dup_fd);
 	close(new_fd);
-	close(dup2_fd);
-
 
 	return 0;
 }
